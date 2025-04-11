3e7169e1686b8f333524710e2175060f84808a64
S.M.Rejuanul Islam <rejuanhossain8@gmail.com>
2025-04-11 18:13:31 +0600

    1st commit

---
+++ script.js
@@ -0,0 +1,176 @@
+/* let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+// console.log(typeof true)
+console.log(typeof javascriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof 'jonas')
+
+
+javascriptIsFun = 'yes!'
+console.log(javascriptIsFun)
+console.log(typeof javascriptIsFun)
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+*/
+
+
+// let, const and var
+// let year = 2001;
+// year = 2002;
+// console.log(year)
+
+// const man = 21;
+// man = 22;
+
+// var world = 'hello';
+// world = 'none';
+// console.log(world)
+
+// Math operator
+// const now = 2025;
+// const ageSamian = now - 1998;
+// const ageRejuan = now - 2001;
+
+// console.log(ageSamian - 2, ageRejuan / 2);
+
+// let firstname = 'Rejuanul';
+// let lastname = 'Islam'
+// console.log(firstname + ' ' + lastname)
+
+
+// Assignment operator
+// let x = 5 + 10;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// console.log(ageSamian > ageRejuan)
+
+
+
+// Coding Challenge #1
+// for data 1
+// task 1
+// let markMass = 78;
+// let markHeight = 1.69; 
+
+// let johnMass =  92;
+// let johnHeight = 1.95; 
+
+// // task 2
+// let marksBMI = markMass / (markHeight * markHeight) ;
+// console.log(marksBMI);
+
+// let johnsBMI = johnMass / (johnHeight * johnHeight) ;
+// console.log(johnsBMI);
+
+
+// // task 3
+// let markHigherBMI = marksBMI > johnsBMI;
+// console.log(markHigherBMI);
+
+
+
+// // for data 2
+// // task 1
+// markMass = 95;
+// markHeight = 1.88; 
+
+// johnMass =  85;
+// johnHeight = 1.76; 
+
+// // task 2
+// marksBMI = markMass / (markHeight * markHeight) ;
+// console.log(marksBMI);
+
+// johnsBMI = johnMass / (johnHeight * johnHeight) ;
+// console.log(johnsBMI);
+
+
+// // task 3
+// markHigherBMI = marksBMI > johnsBMI;
+// console.log(markHigherBMI);
+
+// let firstName = "Rejuanul";
+// let lastName = "Islam";
+// let age = 24;
+// let mylocation = "Chattogram";
+
+// let myself = `My name is ${firstName +" " + lastName}. I am ${age} years old. I live in ${mylocation}.`
+// console.log(myself)
+
+
+// const age = 19;
+
+
+// if(age >= 18){
+//     console.log("Rejuan can drive cars")
+// }
+
+// else {
+//     console.log("Rejuan can't drive")
+// }
+
+
+// const birthYear = 1999;
+
+// let century;
+// if(birthYear <= 2000){
+//     century = 20;
+// }
+// else {
+//     century = 21;
+// }
+
+// console.log(century)
+
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// // Task 1
+// if(BMIMark > BMIJohn){
+//     console.log("Mark's BMI is higher than John's!")
+// }
+
+// else {
+//     console.log("John's BMI is higher than Mark's!")
+// }
+
+// // Task 2
+// if(BMIMark > BMIJohn){
+//     console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`)
+// }
+
+// else {
+//     console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`)
+// }
+
+
+// const inputYear = '1991';
+// console.log(Number(inputYear) + 18)
+
+// let n = '1' + 1; // 11
+// n = n - 1; // 10
+// console.log(n)
+
+
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean('jonas'));
+console.log(Boolean({}));
